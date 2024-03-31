1ca75b2efca65bb10263734270026387b59900f9
Henrique Cordeiro <99266503+HenriqueCordeiroP@users.noreply.github.com>
2024-03-31 11:07:25 -0300

    communication changes

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,68 @@
+import java.io.IOException;
+import java.net.*;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Client implements Runnable {
+    private final MulticastSocket socket = new MulticastSocket(4321);
+    private InetAddress address;
+    private InetSocketAddress socketAddress;
+    private NetworkInterface networkInterface;
+
+    private Client(String serverIp) throws IOException {
+        this.address = InetAddress.getByName(serverIp);
+        this.socketAddress = new InetSocketAddress(this.address, Constants.PORT);
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String outgoingMessage = null;
+        int topic = 0;
+        String name = null;
+
+        System.out.print("Insira seu nome: ");
+        name = sc.nextLine();
+        while(!outgoingMessage.equals("SAIR")) {
+            System.out.print("Tópicos:\n1. Entradas\n2. Pratos Principais\n3. Sobremesas\n" +
+                    "4. Bebidas\n5. Bebidas Alcoólicas\n" +
+                    "Digite o número do tipo do produto que deseja pedir: ");
+            topic = Integer.parseInt(sc.nextLine());
+            if (topic == 1){
+                Client client = new Client(Constants.BAR_ADDRESS);
+                Thread thread = new Thread(client);
+                thread.start();
+            }
+        }
+    }
     @Override
     public void run() {
+        NetworkInterface networkInterface;
+        try {
+            networkInterface = NetworkInterface.getByInetAddress(this.address);
+            this.socket.joinGroup(this.socketAddress, networkInterface);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
+        String incomingMessage = null;
+        while(!Objects.equals(incomingMessage, "FINALIZAR SERVIDOR")){
+            try {
+                byte[] buffer = new byte[1024];
+                DatagramPacket incomingPacket = new DatagramPacket(buffer, buffer.length);
+                this.socket.receive(incomingPacket);
+                incomingMessage = new String(incomingPacket.getData());
+
+                byte[] outgoingMessage = this.formatMessage(incomingMessage).getBytes();
+                DatagramPacket outgoingPacket = new DatagramPacket(
+                        outgoingMessage,
+                        outgoingMessage.length,
+                        this.address,
+                        4321);
+                this.socket.send(outgoingPacket);
+                System.out.println(incomingMessage);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,14 +1,18 @@
 import java.io.IOException;
 import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Server implements Runnable {
-    private MulticastSocket socket;
+    private final MulticastSocket socket = new MulticastSocket(Constants.PORT);;
     private InetAddress address;
+    private InetSocketAddress socketAddress;
 
     private Server(String serverIp) throws IOException{
-        this.socket = new MulticastSocket(Constants.PORT);
         this.address = InetAddress.getByName(serverIp);
+        this.socketAddress = new InetSocketAddress(this.address, Constants.PORT);
     }
     public static void main(String[] args) throws IOException{
         System.out.print("Serviços:\n1. Bar\n2. Restaurante\n" +
@@ -30,16 +34,38 @@ public class Server implements Runnable {
 
     @Override
     public void run() {
-        InetSocketAddress socketAddress = new InetSocketAddress(this.address , Constants.PORT);
         NetworkInterface networkInterface;
         try {
             networkInterface = NetworkInterface.getByInetAddress(this.address);
-            this.socket.joinGroup(socketAddress, networkInterface);
+            this.socket.joinGroup(this.socketAddress, networkInterface);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
+        String incomingMessage = null;
+        while(!Objects.equals(incomingMessage, "FINALIZAR SERVIDOR")){
+            try {
+                byte[] buffer = new byte[1024];
+                DatagramPacket incomingPacket = new DatagramPacket(buffer, buffer.length);
+                this.socket.receive(incomingPacket);
+                incomingMessage = new String(incomingPacket.getData());
 
+                byte[] outgoingMessage = this.formatMessage(incomingMessage).getBytes();
+                DatagramPacket outgoingPacket = new DatagramPacket(
+                        outgoingMessage,
+                        outgoingMessage.length,
+                        this.address,
+                        4321);
+                this.socket.send(outgoingPacket);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private String formatMessage(String message){
+        String moment = new SimpleDateFormat("dd/MM/yyyy - HH:mm").format(new Date());
+        return "[" + moment + "] " + message;
     }
 }
 
